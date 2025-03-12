1e3ba4f0b118e22344707d39190551ccb5b35999
Gereon Tombrink <tombrink@igg.uni-bonn.de>
2025-03-12 09:09:26 +0100

    Add code link to footer

--- trajectopy-react/src/components/Footer.js
+++ trajectopy-react/src/components/Footer.js
@@ -6,6 +6,8 @@ const Footer = () => {
     return <footer className='footer'>
         <div className='footer-items'>
             <a href='https://github.com/gereon-t/trajectopy'>Desktop App</a>
+            <div className='footer-divider'></div>
+            <a href='https://github.com/gereon-t/trajectopy-web'>Code</a>
         </div>
     </footer>
 }
